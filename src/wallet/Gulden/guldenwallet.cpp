frozenflux3/munt-official

commit 6bf6ab8ba98d4e7992f59a80cc263ef5306c4301

Delete both encrypted and uncrypted pubkeys

src/wallet/Gulden/guldenwallet.cpp
@@ -491,9 +491,6 @@ void CGuldenWallet::deleteAccount(CWalletDB& db, CAccount* account, bool shouldP
                     if (IsCrypted())
                     {
                         db.EraseEncryptedKey(pubKey);
-                    }
-                    else
-                    {
                         db.EraseKey(pubKey);
                     }
                 }

commit 66ab8ee0e44140cf67cbd6389d01772c5a2f000d

CORE: Fix minor regression introduced in 6bf6ab8ba98d4e7992f59a80cc263ef5306c4301

src/wallet/Gulden/guldenwallet.cpp
@@ -491,8 +491,8 @@ void CGuldenWallet::deleteAccount(CWalletDB& db, CAccount* account, bool shouldP
                     if (IsCrypted())
                     {
                         db.EraseEncryptedKey(pubKey);
-                        db.EraseKey(pubKey);
                     }
+                    db.EraseKey(pubKey);
                 }
             }
         }